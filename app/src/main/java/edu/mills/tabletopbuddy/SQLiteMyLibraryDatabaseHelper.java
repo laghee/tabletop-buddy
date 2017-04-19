4a41745287778ca7b34d2dbeb83827827e8b2afa
laghee <katrua@gmail.com>
2017-04-19 14:35:00 -0700

    Updated local db with added columns

--- app/src/main/java/edu/mills/tabletopbuddy/SQLiteMyLibraryDatabaseHelper.java
+++ app/src/main/java/edu/mills/tabletopbuddy/SQLiteMyLibraryDatabaseHelper.java
@@ -9,6 +9,7 @@ public class SQLiteMyLibraryDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "mylibrarydatabase"; // the name of our database
     private static final int DB_VERSION = 1; // the version of the database
+    private static final Integer FAVE_INIT = 0; // set favorite to 0 on initial add to database
 
     SQLiteMyLibraryDatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -27,33 +28,40 @@ public class SQLiteMyLibraryDatabaseHelper extends SQLiteOpenHelper {
     private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion < 1) {
             db.execSQL("CREATE TABLE LIBRARY (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "IMAGE TEXT NOT NULL, "
                     + "NAME TEXT NOT NULL, "
+                    + "DESCRIPTION TEXT NOT NULL,"
                     + "THEME TEXT NOT NULL, "
                     + "YEAR_PUBLISHED INTEGER NOT NULL, "
-                    + "RATING DOUBLE NOT NULL, "
+                    + "MY_RATING DOUBLE NOT NULL, "
+                    + "BGG_RATING DOUBLE NOT NULL, "
                     + "MIN_PLAYERS INTEGER NOT NULL, "
                     + "MAX_PLAYERS INTEGER NOT NULL, "
                     + "MIN_TIME INTEGER NOT NULL, "
                     + "MAX_TIME INTEGER NOT NULL, "
-                    + "MIN_AGE INTEGER NOT NULL);");
+                    + "MIN_AGE INTEGER NOT NULL, "
+                    + "FAVORITE INTEGER NOT NULL);");
         }
 
     }
 
-    private static void insertGame(SQLiteDatabase db, String name, String theme, Integer pubdate,
-                                   Double rating, Integer minplayers, Integer maxplayers,
-                                   Integer mintime, Integer maxtime, Integer age) {
+    private static void insertGame(SQLiteDatabase db, String image, String name, String description, String theme, Integer pubdate,
+                                   Double myrating, Double bggrating, Integer minplayers, Integer maxplayers,
+                                   Integer mintime, Integer maxtime, Integer age, FAVE_INIT) {
         ContentValues gameValues = new ContentValues();
+        gameValues.put("IMAGE", image);
         gameValues.put("NAME", name);
-//        drinkValues.put("DESCRIPTION", description);
+        gameValues.put("DESCRIPTION", description);
         gameValues.put("THEME", theme);
         gameValues.put("YEAR_PUBLISHED", pubdate);
-        gameValues.put("RATING", rating);
+        gameValues.put("MY_RATING", myrating);
+        gameValues.put("BGG_RATING", bggrating);
         gameValues.put("MIN_PLAYERS", minplayers);
         gameValues.put("MAX_PLAYERS", maxplayers);
         gameValues.put("MIN_TIME", mintime);
         gameValues.put("MAX_PLAYERS", maxtime);
         gameValues.put("MIN_AGE", age);
+        gameValues.put("FAVORITE", FAVE_INIT);
         db.insert("LIBRARY", null, gameValues);
     }
 
