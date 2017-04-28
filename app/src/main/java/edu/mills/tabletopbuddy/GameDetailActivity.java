43b53da86bcd925e495843cc6664dbd4cc302d12
laghee <katrua@gmail.com>
2017-04-28 14:52:33 -0700

    MyLibraryGameDetail working

--- app/src/main/java/edu/mills/tabletopbuddy/GameDetailActivity.java
+++ app/src/main/java/edu/mills/tabletopbuddy/GameDetailActivity.java
@@ -30,9 +30,11 @@ public class GameDetailActivity extends Activity {
     public static final String EXTRA_CLASSNAME = "class";
     //    private Cursor cursor;
 //    private SQLiteDatabase db;
-    String gameUrl;
+    String gameImageUrl;
     String gameName;
     String gameDescription;
+    String gameThemes;
+    String gamePubYr;
     String playerNum;
     String timeNum;
     String ageNum;
@@ -78,13 +80,11 @@ public class GameDetailActivity extends Activity {
             //Populate the game image
             ImageView photo = (ImageView)findViewById(R.id.photo);
             Log.d("Image: ", fetchedItem.getImageUrl());
-            gameUrl = "https:" + fetchedItem.getImageUrl();
-            gameUrl = gameUrl.substring(0, gameUrl.length() - 4);
-
-            gameUrl = gameUrl.concat("_md.jpg");
-            Log.d("Image: ", gameUrl);
-            Picasso.with(GameDetailActivity.this).load(gameUrl).into(photo);
-
+            gameImageUrl = "https:" + fetchedItem.getImageUrl();
+//            gameImageUrl = gameImageUrl.substring(0, gameImageUrl.length() - 4);
+//            gameImageUrl = gameImageUrl.concat("_md.jpg");
+            Log.d("Image: ", gameImageUrl);
+            Picasso.with(GameDetailActivity.this).load(gameImageUrl).into(photo);
 
             //Populate the game name
             TextView name = (TextView)findViewById(R.id.game_name);
@@ -104,10 +104,6 @@ public class GameDetailActivity extends Activity {
 //            TextView year = (TextView)findViewById(R.id.year);
 //            String year = fetchedItem.getYear();
 //            year.setText(year);
-
-            //Populate the game rating
-//            bggrating = (TextView)findViewById(R.id.rating);
-//            bggrating.setText((CharSequence) fetchedItem.getYear());
 
             //Populate the game min and max players
             TextView players = (TextView)findViewById(R.id.players);
@@ -135,17 +131,17 @@ public class GameDetailActivity extends Activity {
             Log.d("GAME_NO:", Integer.toString(gameId));
 
             try {
-                SQLiteOpenHelper libraryDatabaseHelper = new SQLiteMyLibraryDatabaseHelper(GameDetailActivity.this);
+                SQLiteOpenHelper libraryDatabaseHelper =
+                        new SQLiteMyLibraryDatabaseHelper(GameDetailActivity.this);
                 SQLiteDatabase db = libraryDatabaseHelper.getReadableDatabase();
                 Cursor cursor = db.query("LIBRARY",
-                        new String[]{"NAME", "MIN_PLAYERS", "MAX_PLAYERS", "MIN_TIME",
-                                "MAX_TIME", "MIN_AGE", "BGG_ID", "IMAGE"},
-                        "_id = ?",
+                        new String[]{"IMAGE", "NAME", "DESCRIPTION", "MIN_PLAYERS",
+                                "MAX_PLAYERS", "PLAY_TIME", "MIN_AGE"}, "_id = ?",
                         new String[]{Integer.toString(gameId)},
                         null, null, null);
                 return cursor;
             } catch (SQLiteException e) {
-                Log.d("SQLite Exception: ", e.getMessage());
+                Log.d("LibraryGameDetail: ", "Caught SQLite Exception" + e.getMessage());
                 return null;
             }
 
@@ -153,60 +149,60 @@ public class GameDetailActivity extends Activity {
 
         @Override
         protected void onPostExecute(Cursor cursor) {
-            if (cursor.moveToFirst()) {
-                //Get the game details from the cursor
-                String nameText = cursor.getString(0);
-//                String themeText = cursor.getString(1);
-//                double bggRatingNo = cursor.getDouble(1);
-                int minPlayerNo = cursor.getInt(1);
-                int maxPlayerNo = cursor.getInt(2);
-                int minTimeNo = cursor.getInt(3);
-                int maxTimeNo = cursor.getInt(4);
-                int minAgeNo = cursor.getInt(5);
-                String image = cursor.getString(7);
+            if (cursor != null) {
+                if (cursor.moveToFirst()) {
+                    //Get the game details from the cursor
+                    gameImageUrl = cursor.getString(0);
+                    gameName = cursor.getString(1);
+                    gameDescription = cursor.getString(2);
+//                    gameThemes = cursor.getString(3);
+//                    gamePubYr = cursor.getString(4);
+//              String bggId = cursor.getString(x);
+                    playerNum = cursor.getString(3) + " - " + cursor.getString(4);
+                    timeNum = cursor.getString(5);
+                    ageNum = cursor.getString(6);
 
-//                int photoId = cursor.getInt(8);
 
-                //Populate the game image
-//                photo = (ImageView)findViewById(R.id.photo);
+                    //Populate the game image
+                    ImageView photo = (ImageView) findViewById(R.id.photo);
+                    Log.d("Image: ", gameImageUrl);
+                    Picasso.with(GameDetailActivity.this).load(gameImageUrl).into(photo);
 
-                //Populate the game name
-                TextView name = (TextView)findViewById(R.id.game_name);
-                name.setText(nameText);
+                    //Populate the game name
+                    TextView name = (TextView) findViewById(R.id.game_name);
+                    name.setText(gameName);
 
-                //Populate the game name
-//                theme.setText(themeText);
+                    //Populate the game description
+                    TextView description = (TextView) findViewById(R.id.description);
+                    description.setText(gameDescription);
 
-                //Populate the game rating
-//                rating.setText((int) bggRatingNo);
+                    //Populate the game categories
+                    TextView theme = (TextView) findViewById(R.id.theme);
+                    theme.setText(gameThemes);
 
-                //Populate the game min and max players
-                TextView players = (TextView)findViewById(R.id.players);
-                String playerNum = minPlayerNo + " - " + maxPlayerNo + " players";
-                players.setText(playerNum);
+//                //Populate the game pub year
+//                TextView year = (TextView)findViewById(R.id.year);
+//                year.setText(gamePubYr);
 
-                //Populate the game min and max time
-                TextView time = (TextView)findViewById(R.id.time);
-                String timeNum = minTimeNo + " - " + maxTimeNo + " mins";
-                time.setText(timeNum);
+                    //Populate the game min and max players
+                    TextView players = (TextView) findViewById(R.id.players);
+                    players.setText(playerNum);
 
-                //Populate the game min age
-                TextView minAge = (TextView)findViewById(R.id.ages);
-                String ageNum = "ages: " + minAgeNo + "+";
-                minAge.setText(ageNum);
+                    //Populate the game min and max time
+                    TextView time = (TextView) findViewById(R.id.time);
+                    time.setText(timeNum);
 
-//                //Populate the game image
-                ImageView photo = (ImageView)findViewById(R.id.photo);
-                Picasso.with(GameDetailActivity.this).load(image).into(photo);
-                Log.d("FROM URL", "http:" + image);
+                    //Populate the game min age
+                    TextView minAge = (TextView) findViewById(R.id.ages);
+                    minAge.setText(ageNum);
 
-                //Populate the favorite checkbox
-                CheckBox addToLibrary = (CheckBox)findViewById(R.id.addToLibrary);
-                addToLibrary.setChecked(true);
-            }
-            else {
-                Toast toast = Toast.makeText(GameDetailActivity.this, "Database unavailable", Toast.LENGTH_SHORT);
-                toast.show();
+                    //Populate the favorite checkbox
+                    CheckBox addToLibrary = (CheckBox)findViewById(R.id.addToLibrary);
+                    addToLibrary.setChecked(true);
+                } else {
+                    Toast toast = Toast.makeText(GameDetailActivity.this, "Database unavailable", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         }
     }
@@ -228,7 +224,7 @@ public class GameDetailActivity extends Activity {
         String maxplayers = playersMinMax[1];
 
         ContentValues gameValues = new ContentValues();
-        gameValues.put("IMAGE", gameUrl);
+        gameValues.put("IMAGE", gameImageUrl);
         gameValues.put("NAME", gameName);
         gameValues.put("DESCRIPTION", gameDescription);
 //        gameValues.put("THEME", themeText);
@@ -239,7 +235,6 @@ public class GameDetailActivity extends Activity {
         gameValues.put("MAX_PLAYERS", maxplayers);
         gameValues.put("PLAY_TIME", timeNum);
         gameValues.put("MIN_AGE", ageNum);
-//        db.insert("LIBRARY", null, gameValues);
 
         Log.d("GameDetailActivity", "About to call execute AddGameAsyncTask");
         new AddGameToLibraryTask().execute(gameValues);
