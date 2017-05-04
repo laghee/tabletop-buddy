cc6f5e8942d180d2319ba8ab022f8083993f89dd
kcutler <kristen.cut@gmail.com>
2017-05-04 15:58:30 -0700

    added testclass and one test for LibraryDBUtilities

--- app/src/androidTest/java/edu/mills/tabletopbuddy/LibraryDBUtilitiesTest.java
+++ app/src/androidTest/java/edu/mills/tabletopbuddy/LibraryDBUtilitiesTest.java
@@ -3,10 +3,11 @@ package edu.mills.tabletopbuddy;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.AndroidTestCase;
 import android.test.RenamingDelegatingContext;
+import android.support.test.InstrumentationRegistry;
+
 
 import org.junit.After;
 import org.junit.Before;
@@ -16,9 +17,9 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
-
 public class LibraryDBUtilitiesTest extends AndroidTestCase {
     private SQLiteDatabase db;
+    Game SevenWonders;
 
     @Before
     public void setUp() throws Exception {
@@ -26,36 +27,30 @@ public class LibraryDBUtilitiesTest extends AndroidTestCase {
         RenamingDelegatingContext context =
                 new RenamingDelegatingContext(InstrumentationRegistry.getTargetContext(), "_test");
         db = new SQLiteMyLibraryDatabaseHelper(context).getWritableDatabase();
+        SevenWonders = new Game("http://cf.geekdo-images.com/images/pic860217.jpg", "7 Wonders",
+                "In each age, players receive seven cards from a particular deck, choose one of those " +
+                        "cards, then pass the remainder to an adjacent player. ",
+                        "theme, theme, theme", "12", 2, 7, 30, 3356);
 
-        // Add Games to DB
-        long rowId = GoDatabaseUtilities.addMeeting(db, PROF1_ID, MEETING1_START);
-        GoDatabaseUtilities.endMeeting(db, rowId, MEETING1_TEXT, MEETING1_END);
-        rowId = GoDatabaseUtilities.addMeeting(db, PROF1_ID, MEETING2_START);
-        GoDatabaseUtilities.endMeeting(db, rowId, MEETING2_TEXT, MEETING2_END);
+        LibraryDBUtilities.insertGame(db, SevenWonders);
+    }
 
-        // Create one meeting with PROF2.
-        rowId = GoDatabaseUtilities.addMeeting(db, PROF2_ID);
-        GoDatabaseUtilities.endMeeting(db, rowId, null);
+    @Test
+    public void getGame() throws Exception {
+        int gameId = 0;
+        assertEquals(SevenWonders, LibraryDBUtilities.getGame(db, gameId));
+    }
+
+    @Test
+    public void deleteGame() throws Exception {
+    }
+
+    @Test
+    public void getGameAfterDelete() throws Exception {
     }
 
     @After
     public void takeDown() {
         db.close();
-    }
-
-
-    @Test
-    public void getGame() throws Exception {
-        assertEquals(game, LibraryDBUtilities.get());
-    }
-
-    @Test
-    public void deleteGame() throws Exception {
-        assertEquals(??, LibraryDBUtilities.delete());
-    }
-
-    @Test
-    public void getGameAfterDelete() throws Exception {
-        assertEquals(game, LibraryDBUtilities.get());
     }
 }
